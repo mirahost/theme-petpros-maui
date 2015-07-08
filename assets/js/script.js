3515f82bcd0abc2b8a1e24c05fd92114b8ceaa3e
Stefan Zamfira <stefan.zamfira@osf-global.com>
2015-07-09 01:34:28 +0300

    reset adds images updates and cleaning content page structure updates slider plugin added validator plugin added

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+(function($){
+
+    $('html').removeClass('no-js')
+
+})(jQuery);
+
+
+(function($){
+
+    // Call plugins only on the pages where the plugin is loaded
+    if( typeof $.fn.validate === 'undefined') return false;
+
+    $('#petAdd').validate({
+        ignore : '.ignore',
+        errorElement : 'span'
+    });
+
+})(jQuery);
+
+(function($){
+
+    var $partnersSlider = $('.footer-links .slider');
+
+    // Init partners slider after all images are loaded
+    $(window).on('load', function(){
+        var slideMaxWidth = 0;
+        $('img', $partnersSlider).each(function(){
+            var imgWidth = $(this).width();
+            if( imgWidth > slideMaxWidth ) {
+                slideMaxWidth = imgWidth;
+            }
+        })
+
+        $partnersSlider.bxSlider({
+            minSlides : 4,
+            maxSlides : 6,
+            controls : false,
+            infiniteLoop : false,
+            slideWidth : parseInt( slideMaxWidth, 10 )
+        });
+    });
+
+})(jQuery);
